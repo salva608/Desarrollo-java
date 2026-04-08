70477a118a13b5c311321b6847f5c6632620abd5
salva608 <totoalahielmejor@gmail.com>
2026-04-08 18:04:56 -0500

    Using List.of and Map.of with immutable collections

---
+++ HU3/src/main/java/org/riwi/Task2.java
@@ -0,0 +1,19 @@
+package org.riwi;
+
+import java.util.*;
+
+public class Task2 {
+    public static void main(String[] args) {
+
+        var tecnologias = List.of("Java", "Python", "JavaScript");
+        var sedes = Map.of(
+                "COL", "Colombia",
+                "MX", "México",
+                "USA", "Estados Unidos"
+        );
+
+        System.out.println(tecnologias);
+        System.out.println(sedes);
+
+    }
+}
